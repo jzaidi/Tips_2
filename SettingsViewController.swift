d700220c1a12802b71b123c8bd2e57aa0f1a83fa
jzaidi <Jaffar@Family-MacBook-Pro.local>
2016-01-01 12:08:50 -0500

    Tip Calculator
    Review @codepathreview

---
+++ SettingsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsViewController.swift
+//  Tips_2
+//
+//  Created by Jaffar Zaidi on 1/1/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+    
+    class SettingsViewController: UIViewController, UIPickerViewDelegate {
+}
